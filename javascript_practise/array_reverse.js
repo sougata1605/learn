399502809a58c42b879bbbfb44022c632ebf2f82
Sougata Chatterjee <chatterjee2014@gmail.com>
2025-09-28 10:22:45 +0530

    javascript traverse  string

--- javascript_practise/array_reverse.js
+++ javascript_practise/array_reverse.js
@@ -4,11 +4,18 @@
 
 let array=[12,67,8,90,65,48,40];
 let length=array.length;
-
+let array_original=[];
 
 for(let i=0;i<length;i++){
-    console.log(array[i]);
+    array_original.push(array[i]) ;
 }
+
+console.log("original javascript is as follows "+ array_original);
+
+
+
+
+
 
 
 
---
+++ javascript_practise/array_traverse.js
@@ -0,0 +1,10 @@
+/// traverse an string 
+
+
+let array=[12,67,8,90,65,48,40];
+let length=array.length;
+let array_original=[];
+
+
+/// traverse an array 
+
