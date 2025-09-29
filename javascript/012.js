6a74a68bf08167923028a4a2bf1db39e9e8eb555
Sougata Chatterjee <chatterjee2014@gmail.com>
2025-09-29 12:49:39 +0530

    javascript folder

---
+++ javascript/012.js
@@ -0,0 +1,14 @@
+// Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
+
+let array = [0, 2, 1, 2, 0, 1, 0];
+let high =array.length;
+let min=0;
+let mid=0;
+
+while(mid<=high)
+{
+    if(array[i]===0)
+    {
+        
+    }
+}
