c0f4cec614020b85a5a901183c15c3673335ad6a
Sougata Chatterjee <chatterjee2014@gmail.com>
2025-10-07 17:19:33 +0530

    601

--- javascript_practise/index.js
+++ javascript_practise/index.js
@@ -1,5 +1,32 @@
+/*
 const app= require('./app');
+
+
+var a=12; 
+var b= 23; var c= 46;
+
+console.warn(a+b+c);
+*/ 
+
 const array= [2,4,7,1,3,8,3];
+
+// console.log(array);
+// console.log(array[4]);
+
+
+const mynum= array.forEach((item)=>{
+      //  console.log(item);
+       return item;
+    }
+);
+
+
+ console.log(mynum);
+
+
+
+
+
 
 // let result=array.filter(( ));
 
